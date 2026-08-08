63e9117b5c3ae376677a213b9e2b465a05e6405e
Pravin Yadav <yadavpravin1452@gmail.com>
2026-08-09 01:00:43 +0530

    Add project setup configuration

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='medical-chatbot',
+    version='0.1.0',
+    author='Pravin Yadav',
+    author_email='yadavpravin1452@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
